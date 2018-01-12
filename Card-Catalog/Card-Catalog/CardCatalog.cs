759581c0bd2112fbc2e832c2319f88db20033238
Jim Woodworth <jgwoodworth@gmail.com>
2018-01-12 10:10:49 -0600

    added basic menu option

--- Card-Catalog/Card-Catalog/CardCatalog.cs
+++ Card-Catalog/Card-Catalog/CardCatalog.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Card_Catalog
-{
-    public class CardCatalog
-    {
+//namespace Card_Catalog
+//{
+//    public class CardCatalog
+//    {
+        
+//        private string _filename;
+//        private List<Book> listOfBooks = new List<Book>();         //This is the private member variable that contains all of the books
 
-        private string _filename;
-        private List<Book> listOfBooks = new List<Book>();         //This is the private member variable that contains all of the books
+//        public CardCatalog(string fileName)
+//        {/*code goes here*/
+//        }
 
-        public CardCatalog(string fileName)
-        {/*code goes here*/
-        }
+//        public ?? ListBooks(??)
+//        {/*code goes here*/
+//        }
 
-        public ?? ListBooks(??)
-        {/*code goes here*/
-        }
+//        public ?? AddBook(??)
+//        {/*code goes here*/
+//        }
 
-        public ?? AddBook(??)
-        {/*code goes here*/
-        }
-
-        public ?? Save(??)
-        {/*code goes here*/
-        }
-    }
-
-}
+//        public ?? Save(??)
+//        {/*code goes here*/
+//        }
+  
+//    }
+//}
 
--- Card-Catalog/Card-Catalog/Program.cs
+++ Card-Catalog/Card-Catalog/Program.cs
@@ -6,7 +6,22 @@ namespace Card_Catalog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Amon!");
+            Console.WriteLine("Welcome to the Amon and Jim Card Catalog");
+            Console.WriteLine();
+            Console.WriteLine("Please enter the filename for the card catalog: ");
+            string filename = Console.ReadLine();
+            Console.WriteLine();
+            int menuOption = 0;
+            while (menuOption != 3)
+            {
+                Console.WriteLine("Main Menu: ");
+                Console.WriteLine("  [1] List All Books ");
+                Console.WriteLine("  [2] Add A Book ");
+                Console.WriteLine("  [3] Save and Exit");
+                Console.WriteLine("Please select an option from the menu above: ");
+                menuOption = Convert.ToInt32(Console.ReadLine());
+            }
+ 
         }
     }
 }
