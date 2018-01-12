b56d445a1c758862c6c220c7e3368a009f666aaa
Jim Woodworth <jgwoodworth@gmail.com>
2018-01-12 09:45:09 -0600

    Add classes for book and card caralog

---
+++ Card-Catalog/Card-Catalog/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Catalog
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
---
+++ Card-Catalog/Card-Catalog/CardCatalog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Card_Catalog
+{
+    public class CardCatalog
+    {
+
+        private string _filename;
+        private List<Book> listOfBooks = new List<Book>();         //This is the private member variable that contains all of the books
+
+        public CardCatalog(string fileName)
+        {/*code goes here*/
+        }
+
+        public ?? ListBooks(??)
+        {/*code goes here*/
+        }
+
+        public ?? AddBook(??)
+        {/*code goes here*/
+        }
+
+        public ?? Save(??)
+        {/*code goes here*/
+        }
+    }
+
+}
+
